c4e0fc57dc82d6af3ca02481ddced7618c9a5f52
anna-marie-vos <amvos@outlook.com>
2017-06-28 14:45:56 +1200

    added the about page

--- config/router.js
+++ config/router.js
@@ -4,6 +4,7 @@ import { TabNavigator, StackNavigator } from 'react-navigation'
 import Home from '../screens/Home'
 import NavBar from '../screens/NavBar'
 import NewReport from '../screens/NewReport'
+import About from '../screens/About'
 
 export const NewReportStack = StackNavigator ({
   NewReportP1: {
@@ -23,6 +24,12 @@ export const Tabs = TabNavigator ({
     navigationOptions: {
       tabBarLabel: 'New Report',
     },
+  },
+  AboutSafeAs:{
+    screen: About,
+    navigationOptions:{
+      tabBarLabel: 'About Safe-As'
+    }
   }
 })
 
