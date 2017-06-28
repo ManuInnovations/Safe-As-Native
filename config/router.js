bb4214f4917a4a57a0c1c35b1fef3effadca36c7
MeghanHughes <hughes.meghan@gmail.com>
2017-06-28 13:57:21 +1200

    new report tab renders

--- config/router.js
+++ config/router.js
@@ -3,6 +3,13 @@ import { TabNavigator, StackNavigator } from 'react-navigation'
 
 import Home from '../screens/Home'
 import NavBar from '../screens/NavBar'
+import NewReport from '../screens/NewReport'
+
+// export const NewReport = StackNavigator ({
+//   NewReportP1: {
+//     screen: NewReport,
+//   },
+// })
 
 export const Tabs = TabNavigator ({
   Home: {
@@ -11,6 +18,12 @@ export const Tabs = TabNavigator ({
       tabBarLabel: 'Home',
     }
   },
+  NewReport: {
+    screen: NewReport,
+    navigationOptions: {
+      tabBarLabel: 'New Report',
+    },
+  }
 })
 
 export const Root = StackNavigator ({
