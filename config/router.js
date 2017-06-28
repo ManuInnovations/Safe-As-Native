df513bb74457285af6b53530ddc58c4cbba201a7
MeghanHughes <hughes.meghan@gmail.com>
2017-06-28 13:52:14 +1200

    tab nav bar rendering

--- config/router.js
+++ config/router.js
@@ -2,8 +2,21 @@ import React from 'react'
 import { TabNavigator, StackNavigator } from 'react-navigation'
 
 import Home from '../screens/Home'
+import NavBar from '../screens/NavBar'
+
+export const Tabs = TabNavigator ({
+  Home: {
+    screen: Home,
+    navigationOptions: {
+      tabBarLabel: 'Home',
+    }
+  },
+})
 
 export const Root = StackNavigator ({
+  Tabs: {
+    screen: Tabs,
+  },
   Home: {
     screen: Home,
   },
