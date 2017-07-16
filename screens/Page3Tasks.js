ed288747a044fc994419c17e671c105e51c72a39
anna-marie-vos <amvos@outlook.com>
2017-07-16 17:31:26 +1200

    scrollies work

--- screens/Page3Tasks.js
+++ screens/Page3Tasks.js
@@ -3,6 +3,21 @@ import { StyleSheet, Text, View, TextInput } from 'react-native';
 import Button from 'react-native-button';
 
 export default class page3Report extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      tasks: '',
+    };
+  }
+
+  onTextChange = (tasks) =>{
+    console.log('this is the task', tasks);
+    // this.setState({ tasks })
+  }
+
+  goToNextPage = (user) => {
+    this.props.navigation.navigate('Home');
+  };
 
   render() {
     return (
@@ -10,6 +25,15 @@ export default class page3Report extends React.Component {
         <Text>
           This is the tasks
         </Text>
+        <TextInput
+          onChangeText={(tasks) => this.setState({tasks})}
+          value={this.state.tasks}
+        />
+        <Button
+          onPress={() => this.goToNextPage()}
+        >
+          Next
+        </Button>
       </View>
     );
   }
